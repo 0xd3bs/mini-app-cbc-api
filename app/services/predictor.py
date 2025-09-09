8a7db155a497e666089787c6eef7684b01e9726f
dblanco <dblancove@gmail.com>
2025-09-09 20:07:43 +0000

    fix: temp mock result

--- app/services/predictor.py
+++ app/services/predictor.py
@@ -26,7 +26,7 @@ def get_prediction_values() -> dict:
         prediction_result = onnx_session.run(None, {input_name: input_data})
         
         eth_pred = float(prediction_result[0][0])
-        -trend = 'positive' if eth_pred >= 0 else 'negative'
+        trend = 'positive' if eth_pred >= 0 else 'negative'
         trend = 'positive'
         eth_pred = 1
         
